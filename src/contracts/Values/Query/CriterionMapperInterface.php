1fc0d0ab2f3f14a28d46a88e3483fd948d24d099
mikolaj <mikolaj.adamczyk@ez.no>
2024-09-24 16:47:09 +0200

    Moved Criteria, SortClauses and CriterionMapper

---
+++ src/contracts/Values/Query/CriterionMapperInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Contracts\CoreSearch\Values\Query;
+
+use Doctrine\Common\Collections\Expr\Expression;
+use Ibexa\Contracts\CoreSearch\Values\Query\Criterion\CriterionInterface;
+use Ibexa\CoreSearch\Repository\CriterionMapper;
+
+/**
+ * @template C of \Ibexa\Contracts\CoreSearch\Values\Query\Criterion\CriterionInterface
+ */
+interface CriterionMapperInterface
+{
+    public function canHandle(CriterionInterface $criterion): bool;
+
+    /**
+     * @param C $criterion
+     */
+    public function handle(CriterionInterface $criterion, CriterionMapper $mapper): Expression;
+}
