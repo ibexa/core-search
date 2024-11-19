6cd933c721154c73d387b8cd82f661128e166e56
Andrew Longosz <alongosz@users.noreply.github.com>
2024-11-19 13:47:48 +0100

    [PHPDoc] Fixed PHPStan type-hints

--- src/contracts/Values/Query/AbstractCriterionQuery.php
+++ src/contracts/Values/Query/AbstractCriterionQuery.php
@@ -18,10 +18,10 @@ abstract class AbstractCriterionQuery
 {
     public const int DEFAULT_LIMIT = 25;
 
-    /** @var TCriterion|null */
+    /** @phpstan-var TCriterion|null */
     private ?CriterionInterface $query;
 
-    /** @var TSortClause[] */
+    /** @phpstan-var TSortClause[] */
     private array $sortClauses;
 
     private ?int $limit;
@@ -29,8 +29,8 @@ abstract class AbstractCriterionQuery
     private int $offset;
 
     /**
-     * @param TSortClause[]|null $sortClauses
-     * @param TCriterion|null $query
+     * @phpstan-param TSortClause[]|null $sortClauses
+     * @phpstan-param TCriterion|null $query
      */
     public function __construct(
         ?CriterionInterface $query = null,
@@ -45,7 +45,7 @@ abstract class AbstractCriterionQuery
     }
 
     /**
-     * @param TCriterion|null $criterion
+     * @phpstan-param TCriterion|null $criterion
      */
     final public function setQuery(?CriterionInterface $criterion): void
     {
@@ -53,7 +53,7 @@ abstract class AbstractCriterionQuery
     }
 
     /**
-     * @return TCriterion|null
+     * @phpstan-return TCriterion|null
      */
     final public function getQuery(): ?CriterionInterface
     {
@@ -86,7 +86,7 @@ abstract class AbstractCriterionQuery
     }
 
     /**
-     * @return TSortClause[]
+     * @phpstan-return TSortClause[]
      */
     final public function getSortClauses(): array
     {
