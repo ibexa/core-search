6b2cebce5c1cea5d00efeb6f03e0f5de38ea2713
Adam Wójs <adam@wojs.pl>
2025-05-11 20:09:01 +0200

    [Rector] Added Symfony 7.0..7.2 sets to rector configuration

--- rector.php
+++ rector.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 use Ibexa\Contracts\Rector\Sets\IbexaSetList;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 
 return RectorConfig::configure()
@@ -22,4 +23,7 @@ return RectorConfig::configure()
         SymfonySetList::SYMFONY_62,
         SymfonySetList::SYMFONY_63,
         SymfonySetList::SYMFONY_64,
+        SymfonySetList::SYMFONY_70,
+        SymfonySetList::SYMFONY_71,
+        SymfonySetList::SYMFONY_72,
     ]);
