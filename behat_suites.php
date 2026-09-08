8879b34629c8ebcd6645c36075d3388bb7431031
Dawid Parafiński <dawid.parafinski@ibexa.co>
2026-09-08 16:08:48 +0200

    IBX-12374: Imported the PHP Behat configuration and removed the YAML suites (#24)
    * IBX-12374: Imported the PHP Behat configuration and removed the YAML suites
    
    * IBX-12374: Added the Behat 4 prerelease stability flags to the dev dependencies

--- behat_suites.php
+++ behat_suites.php
@@ -13,7 +13,7 @@ use Ibexa\Behat\API\Context\TestContext;
 use Ibexa\Behat\Browser\Context\AuthenticationContext;
 
 return (new Config())
-    ->import('../../ibexa/behat/behat_ibexa_oss.yaml')
+    ->import('../../ibexa/behat/behat_ibexa_oss.php')
     ->withProfile((new Profile('browser'))
         ->withSuite((new Suite('core-search'))
             ->withContexts(
